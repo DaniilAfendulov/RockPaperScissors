7f9c701f6379a11684cb33b996c7bcbfa59e1a16
Daniil <daniilafendulov@bk.ru>
2021-09-19 19:32:31 +0300

    Modify: RPSGame for non-console using

--- RockPaperScissors/RPSGame.cs
+++ RockPaperScissors/RPSGame.cs
@@ -15,8 +15,9 @@ namespace RockPaperScissors
         
         public RPSGame(string[] args)
         {
-            if (!CheckInput(args))
+            if (!CheckInput(args, out var err))
             {
+                err.ForEach(e => Println(e));
                 Println();
                 Println("The input example:");
                 Println("rock paper scissors lizard Spock");
@@ -24,12 +25,17 @@ namespace RockPaperScissors
             }
             _help = new Command("help", "?");
             _exit = new Command("exit", "0");
-            _moves = CreateMoves(args);
+            InitMoves(args);
             _allCommands = _moves.
                 Select(m => (Command)m).
                 Append(_help).
                 Append(_exit).
                 ToArray();
+        }
+
+        public RPSGame(Move[] moves)
+        {
+            _moves = moves;
         }
 
         public void Start()
@@ -68,7 +74,7 @@ namespace RockPaperScissors
                 if (userCmd == _help)
                 {
                     PrintEnteredCommand(userCmd);
-                    PrintHelpTable(_moves);
+                    Println(GetHelpTable());
                     continue;
                 }
 
@@ -88,28 +94,54 @@ namespace RockPaperScissors
             Println($"HMAC key: {Convert.ToHexString(hmacKey)}");
         }
 
-        private bool CheckInput(string[] input)
+        public static bool CheckInput(string[] input, out List<string> err)
         {
+            err = new List<string>();
             var isRight = true;
             if (input == null || input.Length < 3)
             {
-                Println("the number of input lines must be >= 3");
+                err.Add("the number of input lines must be >= 3");
                 isRight = false;
             }
             if (input.Length % 2 == 0)
             {
-                Println("the number of input lines must be odd");
+                err.Add("the number of input lines must be odd");
                 isRight = false;
             }
             if (input.Distinct().Count() != input.Length)
             {
-                Println("input has duplicate");
+                err.Add("input has duplicate");
                 isRight = false;
             }
             return isRight;
         }
 
-        private Move[] CreateMoves(string[] args)
+        public static string GetCommandsText(Command[] commands)
+        {
+            var result = new StringBuilder();
+            foreach (var cmd in commands)
+            {
+                result.Append($"{cmd.Ch} - {cmd.Name}");
+            }
+            return result.ToString();
+        }
+
+        public string GetCommandsText()
+        {
+            return GetCommandsText(_moves);
+        }
+
+        public static string GetHelpTable(Move[] moves)
+        {
+            var table = new HelpTable(moves);
+            return table.ToString();
+        }
+        public string GetHelpTable()
+        {
+            return GetHelpTable(_moves);
+        }
+
+        public static Move[] CreateMoves(string[] args)
         {
             var moves = new List<Move>();
             for (int i = 0; i < args.Length; i++)
@@ -119,6 +151,11 @@ namespace RockPaperScissors
             return moves.ToArray();
         }
 
+        private void InitMoves(string[] args)
+        {
+            _moves = CreateMoves(args);
+        }
+
         private void Print(string msg)
         {
             Console.Write(msg);
@@ -126,10 +163,7 @@ namespace RockPaperScissors
 
         private void PrintCommands(Command[] commands)
         {
-            foreach (var cmd in commands)
-            {
-                Println($"{cmd.Ch} - {cmd.Name}");
-            }
+            Println(GetCommandsText(commands));
         }
 
         private void Println(string msg)
@@ -163,7 +197,7 @@ namespace RockPaperScissors
             Println("Your command: " + command.Name);
         }
 
-        private Move CompMove(Move[] moves)
+        private static Move CompMove(Move[] moves)
         {
             var number = RandomNumberGenerator.GetInt32(moves.Length);
             return moves[number];
@@ -174,7 +208,7 @@ namespace RockPaperScissors
             return Console.ReadLine();
         }
 
-        private bool TryFindCommand(string input, Command[] commands, out Command command)
+        private static bool TryFindCommand(string input, Command[] commands, out Command command)
         {
             bool isFind = false;
             var chCmds = commands.ToDictionary(c => c.Ch);
